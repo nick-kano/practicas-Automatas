d134043208de77f65aa96089e24f04baf23dbf93
Emiliano2123 <emilianodc@ciencias.unam.mx>
2022-01-22 17:03:22 -0600

    Entrada estandar de cadena

--- practica2/Menu.py
+++ practica2/Menu.py
@@ -1,36 +1,33 @@
 import cinta
 import MT
 import transicion
-import Js
+import Js as JS
 import sys
 
 class Menu:
     
-    j = JsonUwU(sys.argv[1])
+    j = JS.JsonUwU(sys.argv[1])
     switch = False
-    while switch == False: #revisa que la cadena si tenga los simbolos que aceptamos
-        i = 0
-        cadena = input("Favor de ingresar la cadena de entrada")
-        for x in cadena:
-            if not(x in j.cinta):
-                print("Esa cadena no es valida, favor de ingresar una que lo sea")
-                break
-            else:
-                i+=1
-                if(i == cadena.length()-1):
-                    switch = true
-
-    c = Cinta(cadena,json.blanco)
-    t = Transicion(json.transicion)
-    mt = MaquinaTuring(json.estados,json.entrada,c,json.inicial,json.finales,json.blanco,t)
     i = 0
-    print("Las configuraciones son : ")
-    print("| " + mt.estadoActual + " | " + mt.cinta.entrada) #No se si sea necesario poner la configuracion inicial, pero esta es una forma fea de que lo haga
-    while (i == 0):
-        i = mt.mover()
-        print("| " + mt.estadoActual + " | " + mt.cinta.entrada) #La idea es que imprima el estado y la cadena en cada paso
-    if(i == -1):
-        print("No es aceptada")
+    cadena = sys.argv[2]
+    for x in cadena:
+        if (x in j.cinta):
+            i+=1
+            if(i == cadena.length()-1):
+                switch = true
+    if(switch == true):
+        c = Cinta(cadena,json.blanco)
+        t = Transicion(json.transicion)
+        mt = MaquinaTuring(json.estados,json.entrada,c,json.inicial,json.finales,json.blanco,t)
+        i = 0
+        print("Las configuraciones son : ")
+        print("| " + mt.estadoActual + " | " + mt.cinta.entrada) #No se si sea necesario poner la configuracion inicial, pero esta es una forma fea de que lo haga
+        while (i == 0):
+            i = mt.mover()
+            print("| " + mt.estadoActual + " | " + mt.cinta.entrada) #La idea es que imprima el estado y la cadena en cada paso
+        if(i == -1):
+            print("No es aceptada")
+        else:
+            print("Es aceptada")
     else:
-        print("Es aceptada")
-
+        print("Esa cadena no es valida")
